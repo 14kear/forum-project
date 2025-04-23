660b8cd01b63e888a52a4f42f10e636242bfc4ea
toose666 <kira410048@inbox.ru>
2025-04-24 01:50:21 +0400

    logs added, server start add

---
+++ auth-service/cmd/auth/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"github.com/14kear/forum-project/auth-service/internal/config"
+	"github.com/14kear/forum-project/auth-service/internal/lib/logger/handlers/slogpretty"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envProd  = "prod"
+	envDev   = "dev"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("Starting up auth service", slog.Any("config", cfg)) // временно, УБРАТЬ В БУДУЩЕМ
+
+	// TODO: инициализировать приложение (app)
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
+}
