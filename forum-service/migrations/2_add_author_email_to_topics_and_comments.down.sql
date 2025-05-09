539f49404f067aa98546afb8b52dafb6c7476848
toose666 <kira410048@inbox.ru>
2025-05-10 02:12:37 +0400

    forum-service and auth-service done !

---
+++ forum-service/migrations/2_add_author_email_to_topics_and_comments.down.sql
@@ -0,0 +1,2 @@
+ALTER TABLE topics DROP COLUMN author_email;
+ALTER TABLE comments DROP COLUMN author_email;
---
+++ forum-service/migrations/2_add_author_email_to_topics_and_comments.up.sql
@@ -0,0 +1,2 @@
+ALTER TABLE topics ADD COLUMN author_email TEXT;
+ALTER TABLE comments ADD COLUMN author_email TEXT;
